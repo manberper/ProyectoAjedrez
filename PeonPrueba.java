b21346f24734ca7047095a7e6feb0f246cdbc6c6
Carlos-Gamrra554 <cargamtom@alu.edu.gva.es>
2024-12-16 08:44:22 +0100

    Peón terminado (creo)

--- PeonPrueba.java
+++ PeonPrueba.java
@@ -1,59 +1,50 @@
 public class PeonPrueba {
     private String posicion;
     private String color;
-    private char columna = posicion.charAt(0);
-    private int fila = posicion.charAt(1);
+    private char columna;
+    private int fila;
 
     public PeonPrueba(String posicion, String color) {
-        if (fila < 0 || fila > 7 || columna < 0 || columna > 7) {
-            throw new IllegalArgumentException("Posición fuera de los límites del tablero.");
-        }
-
         this.posicion = posicion;
         this.color = color;
+        this.columna = posicion.charAt(0);
+        this.fila = Character.getNumericValue(posicion.charAt(1));
+
+        if (fila < 0 || fila > 8 || columna < 'a' || columna > 'h') {
+            throw new IllegalArgumentException("Posición fuera de los límites del tablero.");
+        }
     }
 
     public String[] calculaMovimiento() {
-        int numero = Integer.MAX_VALUE;
-
-        switch (columna) {
-            case 'a':
-                numero = 1;
-                break;
-            case 'b':
-                numero = 2;
-                break;
-            case 'c':
-                numero = 3;
-                break;
-            case 'd':
-                numero = 4;
-                break;
-            case 'e':
-                numero = 5;
-                break;
-            case 'f':
-                numero = 6;
-                break;
-            case 'g':
-                numero = 7;
-                break;
-            case 'h':
-                numero = 8;
-                break;
-        }
-
-        String posicionNumeros = Integer.toString(numero) + Integer.toString(fila);
+        int numero = columna - 'a' + 1;
+        int filaActual = this.fila;
         int contadorMovimientos = 0;
 
-        if (fila == 2 && color.equals("blanco") || fila == 7 && color.equals("negro")) {
+        if ((fila == 2 && color.equals("blanco")) || (fila == 7 && color.equals("negro"))) {
             contadorMovimientos = 2;
         } else {
             contadorMovimientos = 1;
+            this.fila += 1;
         }
 
         String[] movimientos = new String[contadorMovimientos];
+        filaActual += 1;
+        movimientos[0] = String.valueOf((char) ('a' + numero - 1)) + filaActual;
 
+        if (contadorMovimientos > 1) {
+            filaActual += 1;
+            movimientos[1] = String.valueOf((char) ('a' + numero - 1)) + filaActual;
+            ;
+        }
         return movimientos;
+    }
+
+    public static void print(String[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+            if (i < array.length - 1) {
+                System.out.print(", ");
+            }
+        }
     }
 }
