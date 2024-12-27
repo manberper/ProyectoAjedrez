c344dd8795bd6f05a827b6c9372ddf40e59c8b18
manberper <manberper@alu.edu.gva.es>
2024-12-27 16:15:15 +0100

    Pruebas con peon2, devuelve bien sus movimientos

--- CalculadoraAjedrez/src/Peon2.java
+++ CalculadoraAjedrez/src/Peon2.java
@@ -20,20 +20,20 @@ public class Peon2 {
         int filaInicio = color.equals("blanco") ? 2 : 7;
 
         String[] posiblesMovimientos = new String[2];
-        int index = 0;
+        int cont = 0;
 
         // Movimiento de 1 casilla
         if (tablero.esPosicionValida(fila + direccion, columna)) {
-            posiblesMovimientos[index++] = tablero.obtenerPosicion(fila + direccion, columna);
+            posiblesMovimientos[cont++] = tablero.obtenerPosicion(fila + direccion, columna);
         }
 
         // Movimiento de 2 casillas desde la fila inicial
         if (fila == filaInicio && tablero.esPosicionValida(fila + 2 * direccion, columna)) {
-            posiblesMovimientos[index++] = tablero.obtenerPosicion(fila + 2 * direccion, columna);
+            posiblesMovimientos[cont++] = tablero.obtenerPosicion(fila + 2 * direccion, columna);
         }
 
         // Filtrar movimientos nulos
-        return filtrarMovimientos(posiblesMovimientos, index);
+        return filtrarMovimientos(posiblesMovimientos, cont);
     }
 
     private String[] filtrarMovimientos(String[] movimientos, int size) {
