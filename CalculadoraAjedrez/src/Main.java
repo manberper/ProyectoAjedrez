e9f41e84181d7371d5908dcd70b6ad5eba1480c3
SandraBerna06 <sanberzap@alu.edu.gva.es>
2024-12-27 18:37:52 +0100

    Main actualizado

--- CalculadoraAjedrez/src/Main.java
+++ CalculadoraAjedrez/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -7,69 +8,79 @@ public class Main {
 
         System.out.println("Bienvenido al calculador de movimientos de ajedrez.");
 
-            //Elegir pieza
-            System.out.println("Elige una de las siguientes piezas: (Torre, Peon, Caballo, Alfil, Rey, Reina");
-            String pieza = sc.nextLine().toLowerCase();
+        //Elegir pieza
+        System.out.println("Elige una de las siguientes piezas: (Torre, Peon, Caballo, Alfil, Rey, Reina)");
+        String pieza = sc.nextLine().toLowerCase();
 
-            //Elegir color
-            System.out.println("Seleccione el color que desee: (Blanco/Negro");
-            String color = sc.nextLine().toLowerCase();
+        //Elegir color
+        System.out.println("Seleccione el color que desee: (Blanco/Negro)");
+        String color = sc.nextLine().toLowerCase();
 
-            //Elegir la posición
-            System.out.println("Introduce la columna de la pieza (A-H): ");
-            char columnaChar = sc.next().charAt(0);
+        //Elegir la posición
+        System.out.println("Introduce la columna de la pieza (A-H): ");
+        char columnaChar = sc.next().charAt(0);
 
 
-            System.out.println("Introduce el número de la fila de la pieza (1-8): ");
-            int fila = sc.nextInt();
+        System.out.println("Introduce el número de la fila de la pieza (1-8): ");
+        int fila = sc.nextInt();
 
         Tablero tablero = new Tablero();
+        String piezaUnicode = "";
         int columna = columnaChar - 'a' + 1;
-            switch (pieza) {
-                case "peon":
-                    //Instancia peón
-                    Peon peon= new Peon(color, fila, columna);
-                    peon.movimientosPosibles();
-                    break;
+        switch (pieza) {
+            case "peon":
+                // Se importa la pieza y se imprimen los movimientos posibles
+                Peon2 peon2= new Peon2(color, fila, columna);
+                System.out.println("Los movimientos posibles son: "+ Arrays.toString(peon2.getPosiciones(tablero)));
+                if (color.equals("blanco")){
+                    piezaUnicode = "♟";
+                }
+                else piezaUnicode = "♙";
+                System.out.println();
+                // Usamos el tablero para imprimir una representación visual
+                tablero.imprimirTablero(fila,columna,peon2.getPosiciones(tablero),piezaUnicode);
 
-                case "torre":
+                break;
 
-                    break;
+            case "torre":
 
-                case "alfil":
-                    //Instancia alfil
-                    Alfil alfil= new Alfil(columna, fila, color);
-                    alfil.mostrarMovimientos();
-                    break;
+                break;
 
-                case "caballo":
-                    //Instancia caballo
-                    Caballo caballo= new Caballo(color, fila, columna);
+            case "alfil":
+                //Instancia alfil
+                Alfil alfil= new Alfil(columna, fila, color);
+                alfil.mostrarMovimientos();
+                break;
 
-                    //Obtener posibles posiciones
-                    String[] posicionesCaballo = caballo.getPosiciones(tablero);
+            case "caballo":
+                //Instancia caballo
+                Caballo caballo= new Caballo(color, fila, columna);
+                System.out.println("Los movimientos posibles son: "+ Arrays.toString(caballo.getPosiciones(tablero)));
 
-                    // Mostrar las posiciones posibles
-                    System.out.println("Las posiciones posibles para el caballo son:");
-                    for (String posicion : posicionesCaballo) {
-                        System.out.println(posicion);
-                    }
-                    break;
+                if (color.equals("blanco")){
+                    piezaUnicode = "♞";
+                }
+                else piezaUnicode = "♘";
+                System.out.println();
+                // Usamos el tablero para imprimir una representación visual
+                tablero.imprimirTablero(fila,columna,caballo.getPosiciones(tablero),piezaUnicode);
 
-                case "reina":
+                break;
 
-                    break;
+            case "reina":
 
-                case "rey":
-                    //Instancia rey
-                    Rey rey = new Rey(columna, fila, color);
-                    rey.mostrarMovimientos();
-                    break;
+                break;
 
-                default:
-                    System.out.println("Pieza invalida");
-                    break;
-            }
+            case "rey":
+                //Instancia rey
+                Rey rey = new Rey(columna, fila, color);
+                rey.mostrarMovimientos();
+                break;
+
+            default:
+                System.out.println("Pieza invalida");
+                break;
+        }
         }
     }
 
